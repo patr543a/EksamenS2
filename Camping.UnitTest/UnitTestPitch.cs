33a410e157e75b4113b1295765472c8dbbd59d33
patr543a <113888937+patr543a@users.noreply.github.com>
2023-03-08 14:13:04 +0100

    Added UnitTests

---
+++ Camping.UnitTest/UnitTestPitch.cs
@@ -0,0 +1,36 @@
+﻿using Camping.Entities.DataClasses;
+
+namespace Camping.UnitTest
+{
+    public class UnitTestPitch
+    {
+        [Fact]
+        public void Pitch_New_AssertsTrue()
+        {
+            _ = new Pitch(0, "Valid");
+
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Pitch_Mutation_AssertsTrue()
+        {
+            var p = new Pitch(0, "Valid");
+
+            p.PitchName = "x";
+
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Pitch_Mutation_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var p = new Pitch(0, "Valid");
+
+                p.PitchName = "";
+            });
+        }
+    }
+}
