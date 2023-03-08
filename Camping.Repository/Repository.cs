e7f7c51a0efa2da241cb140cf403b0bce8dc7da6
patr543a <113888937+patr543a@users.noreply.github.com>
2023-03-08 14:12:03 +0100

    Added functionality to add data to the database

--- Camping.Repository/Repository.cs
+++ Camping.Repository/Repository.cs
@@ -2,6 +2,7 @@
 using Camping.Entities.Interfaces;
 using Camping.Entities.Utility;
 using Camping.Services;
+using System.ComponentModel.DataAnnotations;
 using System.Data.SqlClient;
 
 namespace Camping.Repository
@@ -56,6 +57,8 @@ namespace Camping.Repository
             // Get sql command string to use
             var query = type switch
             {
+                Booking => "select * from Bookings",
+                Customer => "select * from Customers",
                 Pitch => "select * from Pitches",
                 _ => string.Empty
             };
@@ -85,8 +88,48 @@ namespace Camping.Repository
                 list.Add((T)data);
             }
 
+            // Dispose cmd
+            cmd.Dispose();
+
             // Return list
             return list;
+        }
+
+        /// <summary>
+        /// Adds the given object of type T to the database
+        /// </summary>
+        /// <typeparam name="T">Type to add</typeparam>
+        /// <param name="username">Username used to validate access</param>
+        /// <param name="password">Password used to validate access</param>
+        /// <param name="type">The type of IDataClass to add to the database</param>
+        /// <param name="o">Object to add to the datatbase</param>
+        /// <exception cref="ArgumentException"></exception>
+        public void AddDataClass<T>(string username, string password, T type, object o)
+            where T : IDataClass
+        {
+            // Validate login
+            LoginService.Verify(username, password);
+
+            // Get sql command string to use
+            var query = type switch
+            {
+                Booking => "insert into Bookings values (@C1, @C2, @C3, @C4, @C5, @C6, @C7)",
+                Customer => "insert into Customers values (@C1, @C2, @C3, @C4)",
+                Pitch => "insert into Pitches values (@C1, @C2)",
+                _ => string.Empty
+            };
+
+            // Throw if command string is empty
+            if (query == string.Empty)
+                throw new ArgumentException("The given type was no valid", nameof(type));
+
+            // Get SqlCommand and get SqlDataReader
+            var cmd = HelperClass.GetSqlCommand(query, _sqlConnection);
+
+            HelperClass.AddDataClass(cmd, type, o);
+
+            // Dispose cmd
+            cmd.Dispose();
         }
 
         /// <summary>
